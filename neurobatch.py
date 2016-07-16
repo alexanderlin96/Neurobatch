af1e49b7433a0dea67b16dc64ca0c5decdcf5305
alexanderlin96 <alexanderlin@college.harvard.edu>
2016-07-16 15:35:29 -0400

    Add files via upload

--- neurobatch.py
+++ neurobatch.py
@@ -29,6 +29,7 @@ def directory_exists(parser, arg):
 	return arg
  
 def f(x):
+	global mydict
 	x_coor_raw = str(mydict[x][0])
 	y_coor_raw = str(mydict[x][1])
 	z_coor_raw = str(mydict[x][2])
@@ -36,7 +37,6 @@ def f(x):
 	# this path returns us a json which includes the download link
 	path = "http://neurosynth.org/locations/"+ x_coor_raw + "_" + y_coor_raw + "_" + z_coor_raw + "/images"
 	success = False
-	testfile = urllib.request.URLopener()
 	fc = ""
 	mc = ""
 	while(not success):
@@ -56,10 +56,49 @@ def f(x):
 	mutex.acquire()
 	try:
 		global progress
+		mydict[x] = fields
 		progress += 1
 	finally:
 		mutex.release()
 	return fields
+
+def y(x, args):
+	x_coor_raw = str(mydict[x][0])
+	y_coor_raw = str(mydict[x][1])
+	z_coor_raw = str(mydict[x][2])
+	download_link = str(mydict[x][3])
+	success_inner = False
+	while(not success_inner):
+
+		wait_time = random.randint(args.bounds[0],args.bounds[1]);
+		# print('Waiting for: '+str(wait_time)+' seconds', end="", flush=False)
+		for i in range(wait_time):
+			print('.', end="",flush=False)
+			time.sleep(1)
+		# print('X: '+x_coor_raw.zfill(2)+' Y: '+y_coor_raw.zfill(2)+' Z: '+z_coor_raw.zfill(2))
+		# print('Download: '+download_link)
+
+		folder = ''
+		if(args.outputfolder != ''):
+			folder = args.outputfolder;
+
+		filename = folder+x_coor_raw.zfill(2)+'_'+y_coor_raw.zfill(2)+'_'+z_coor_raw.zfill(2)+'.nii.gz'
+		# print("Saving to file: "+filename)
+		try: urllib.request.URLopener().retrieve(download_link, filename)
+		except urllib.error.URLError as e:
+			print(e.reason)
+			continue; 
+		end = time.time();
+		success_inner = True;
+		# return 'X: '+x_coor_raw.zfill(2)+' Y: '+y_coor_raw.zfill(2)+' Z: '+z_coor_raw.zfill(2)+ ' done'
+		mutex.acquire()
+		try:
+			global progress
+			progress += 1
+		finally:
+			mutex.release()
+		
+
 
 def write_to_csv(writer,fields):
 	writer.writerow(fields);
@@ -74,7 +113,6 @@ def progress_bar():
 		try:
 			global progress
 			global total
-			global progress_x
 			percentage = '{percent:.2%}'.format(percent=progress/total)
 			x = int((progress/total) * 40)
 			# sys.stdout.write(chr(8)*(41 + len(percentage)))
@@ -87,6 +125,7 @@ def main():
 	parser = argparse.ArgumentParser(description="Batch Donwloader for http://neurosynth.org Functional Connectivity images")
 	parser.add_argument("-i", dest="inputcsv", required=True, help="csv file path", metavar="FILE", type=lambda x: is_valid_file(parser, x))
 	parser.add_argument("-o", dest="outputfolder", help="output folder path", metavar="FILE", type=lambda x: directory_exists(parser, x))
+	parser.add_argument("-a", dest="workers", required=False, help="multithread mode", type=int);
 	parser.add_argument("-w", dest="bounds", nargs=2, metavar="SEC", required=True, type=int, help="random wait time lower and upper bounds inclusive");
 	args = parser.parse_args()
 
@@ -97,6 +136,10 @@ def main():
 	lowerbound = args.bounds[0]
 	upperbound = args.bounds[1]
 
+	workers = 1
+	if(args.workers is not 0 and args.workers is not None):
+		workers = args.workers
+
 	f1 = open(args.inputcsv,'r')
 	f2 = open('tmp_'+args.inputcsv, 'a');
 
@@ -104,14 +147,18 @@ def main():
 	for count, elem in enumerate(csv_reader):
 		total += 1
 		mydict[count] = elem
-	f1.close();	
-	print('dictdone')
+	f1.close();
+	print()	
+	print('Loaded coordinates into Memory')
+	print()
+
+	print('Collecting Download Links')
 
 	csv_writer = csv.writer(f2);
 
 	progress_bar()
 
-	with ThreadPoolExecutor(max_workers=10) as executor:
+	with ThreadPoolExecutor(max_workers=workers) as executor:
 		futures = [executor.submit(f, key) for key in mydict.keys()]
 		for future in as_completed(futures):
 			if(future.exception() is not None):
@@ -126,6 +173,7 @@ def main():
 	f2.close();
 
 	collecting_info = False;
+	print()
 	print('Done collecting download links')
 	print()
 	
@@ -134,51 +182,28 @@ def main():
 	os.rename('tmp_'+args.inputcsv,args.inputcsv)
 
 	if(args.outputfolder == None):
+		print(mydict)
 		quit()
 	elif(args.outputfolder != '' and args.outputfolder != None):
 		# makes the folder the files are going to be in
 		os.makedirs(args.outputfolder)
 
-	print('Initial File Downloads...')
+	print('Initiating File Downloads with '+str(workers)+' worker(s)')
+	collecting_info = True
+	progress = 0
+	progress_bar()
+
+	with ThreadPoolExecutor(max_workers=workers) as executor:
+		futures = [executor.submit(y, key, args) for key in mydict.keys()]
+		for future in as_completed(futures):
+			if(future.exception() is not None):
+				print(future.exception());
+
+	collecting_info = False			
+	sys.stdout.write('Progress' + ": [" + "#" * 40 + "] 100.00%"  +'\n')
+	sys.stdout.flush()
 	print()
-
-	f3 = open(args.inputcsv,'r')
-	csv_download = csv.reader(f3)
-
-	for row in csv_download:
-
-		x_coor_raw = str(row[0])
-		y_coor_raw = str(row[1])
-		z_coor_raw = str(row[2])
-		download_link = str(row[3])
-		success_inner = False
-		while(not success_inner):
-
-			wait_time = random.randint(lowerbound,upperbound);
-			print('Waiting for: '+str(wait_time)+' seconds', end="", flush=False)
-			for i in range(wait_time):
-				print('.', end="",flush=False)
-				time.sleep(1)
-			print();
-			print('X: '+x_coor_raw.zfill(2)+' Y: '+y_coor_raw.zfill(2)+' Z: '+z_coor_raw.zfill(2))
-			print('Downloading: '+download_link)
-
-			folder = ''
-			if(args.outputfolder != ''):
-				folder = args.outputfolder;
-
-			filename = folder+x_coor_raw.zfill(2)+'_'+y_coor_raw.zfill(2)+'_'+z_coor_raw.zfill(2)+'.nii.gz'
-			print("Saving to file: "+filename)
-			testfile = urllib.request.URLopener()
-			try: testfile.retrieve(download_link, filename)
-			except urllib.error.URLError as e:
-				print(e.reason)
-				continue; 
-			end = time.time();
-			success_inner = True;
-			print()
- 
-####################
+	print('All files downloaded successfully')
  
 if __name__ == "__main__":
-    main()
+	main()
